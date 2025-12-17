eaca2601077f25146a343540c1129fbaaec59fb4
Turkes, Medin <medin.turkes@realcore.de>
2025-12-17 14:02:50 +0100

    Add script

--- src/App.jsx
+++ src/App.jsx
@@ -76,3 +76,12 @@ function NavigationHandler() {
 
   return null;
 }
+
+function setVh() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVh();
+window.addEventListener('resize', setVh);
+
